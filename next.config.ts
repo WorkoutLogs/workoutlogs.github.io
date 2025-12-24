b67bf1dd9cb4ef3cb9e93f97d3e45f4aa166da0a
ReddyyZ <arthurc.oli.whitehat@gmail.com>
2025-12-24 13:12:31 -0300

    build: setup github pages

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
+
+  /**
+   * Disable server-based image optimization. Next.js does not support
+   * dynamic features with static exports.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
+   */
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
